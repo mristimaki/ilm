643360db7c257a3a91e71eaf7747c83baceff198
mristimaki <rosanna.ristimaki@gmail.com>
2023-03-15 10:42:03 +0000

    Add function to change innerHTML in container when changing-button is clicked.

--- assets/js/script.js
+++ assets/js/script.js
@@ -5,15 +5,22 @@
 const container = document.getElementById('container');
 const topArea = document.getElementById('top-area');
 const bottomArea = document.getElementById('bottom-area');
-const button = document.getElementsByClassName('changing-button');
+const changingButton = document.getElementsByClassName('changing-button');
 
-
+// function to empty content of container when "start quiz" is ckicked
 function startQuiz() {
+    topArea.innerHTML = '';
+    bottomArea.innerHTML = '';
+    changingButton.innerHTML = '';
+
+    const question = '<h3>This is the first statement...</h3>';
+    const options = '<p>First option</p> <br> <p>Second option</p> <br> <p>Third option</p>';
+    const button = '<p>Score: <span id="increment-score">0</span></p>';
+
+    container.innerHTML += question;
+    container.innerHTML += options;
+    container.innerHTML += button;
     
-}
-
-function displayQuestion() {
-
 }
 
 function checkCorrectAnswer() {
