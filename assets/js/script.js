45a5e8816f60d5a877a5c939e7c633d8f36d7a13
mristimaki <rosanna.ristimaki@gmail.com>
2023-03-15 03:08:50 +0000

    Trying to add event listeners to hide content, not working yet

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,5 +1,15 @@
+let button = document.getElementsByClassName('top-area', 'bottom-area');
+var display = 0;
+
 function startQuiz() {
 
+    if(display == 1) {
+        button.style.display = 'block';
+        display = 0;
+    } else {
+        button.style.display = 'none';
+        display = 1;
+    }
 }
 
 function displayQuestion() {
@@ -15,5 +25,5 @@ function incrementScore() {
 }
 
 function nextQuestion() {
-    
+
 }
