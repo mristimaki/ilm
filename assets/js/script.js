4090860173a6bc3c558425eb7da0e7779ddf5557
mristimaki <rosanna.ristimaki@gmail.com>
2023-03-15 18:29:10 +0000

    Add score-area and play-again button along with style.

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,4 +1,4 @@
-/**
+/*
  * Declaring constants for DOM elements
  * and possible choices.
  */
@@ -6,20 +6,31 @@ const container = document.getElementById('container');
 const start = document.getElementById('start');
 const quiz = document.getElementById('quiz');
 const result = document.getElementById('result');
-const changingButton = document.getElementById('changing-button');
+const startButton = document.getElementById('start-button');
+const scoreArea = document.getElementById('score-area');
+const playAgain = document.getElementById('play-again');
 
-//hide quiz and result area when window is loaded
-window.onload = function startPage() {
+ /* 
+ * hide quiz and result area when window is loaded and 
+ * display the score-area
+ */
+ window.onload = function startPage() {
     quiz.style.display = 'none';
     result.style.display = 'none';
+    scoreArea.style.display = 'none';
+    playAgain.style.display = 'none';
 }
 
-// function to show quiz area and hide start and result area 
-// when "start quiz" is ckicked
+/*
+ * function to show quiz area and hide start and result area 
+ * when "start quiz" is ckicked
+ */
 function startQuiz() {
     quiz.style.display = 'contents';
+    scoreArea.style.display = 'contents';
     start.style.display = 'none';
     result.style.display = 'none';
+    startButton.style.display = 'none';
 }
 
 function checkCorrectAnswer() {
