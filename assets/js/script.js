684451e6538d1748ca956280d767403c5ace052e
mristimaki <rosanna.ristimaki@gmail.com>
2023-03-17 08:35:24 +0000

    Add function to run the game and collect random answers from questions array, not complete yet

--- assets/js/script.js
+++ assets/js/script.js
@@ -7,11 +7,21 @@ const start = document.getElementById('start');
 const quiz = document.getElementById('quiz');
 const result = document.getElementById('result');
 const startButton = document.getElementById('start-button');
-const box = document.getElementById('box');
+const questions = document.getElementById('question');
+const choices = Array.from(document.getElementsByClassName('choices'));
 const scoreArea = document.getElementById('score-area');
-const choiceQue = document.getElementsByClassName('abc');
+const currentScore = document.getElementById('current-score');
+const totalScore = document.getElementById('total-score');
 const nextButton = document.getElementById('next-button');
 const playAgain = document.getElementById('play-again');
+
+let currentQuestion = {};
+let acceptingAnswers = true;
+let score = 0;
+let questionCounter = 0;
+let availableQuestions = [];
+const SCORE_POINTS = 1;
+const MAX_QUESTIONS = 10;
 
  /* 
  * hide quiz and result area when window is loaded and 
@@ -22,36 +32,23 @@ const playAgain = document.getElementById('play-again');
     result.style.display = 'none';
     scoreArea.style.display = 'none';
     playAgain.style.display = 'none';
-}
-
-/*
- * function to show quiz area and hide start and result area 
- * when "start quiz" is ckicked
- */
-function startQuiz() {
-    quiz.style.display = 'contents';
-    scoreArea.style.display = 'contents';
-    start.style.display = 'none';
-    result.style.display = 'none';
-    startButton.style.display = 'none';
-}
-
+;}
 
 /**
  * questions and answers
  */
-let myQuestions = [{
-    question: "This is the first statement..",
-    option1: "This is the first option.",
-    option2: "This is the second option.",
-    option3: "This is the third option.",
+let questions = [{
+    question: "Which sahaba did Prophet Muhammad (ﷺ) help to become free from being a slave by planting 300+ date palm trees?",
+    option1: "Bilal ibn Rabah",
+    option2: "Salman al Farisi",
+    option3: "Zayd ibn Harisa",
     answer: 2
 },  
     {
-        question: "This is the second statement..",
-    option1: "This is the first option.",
-    option2: "This is the second option.",
-    option3: "This is the third option.",
+        question: "Which Angel will blow the horn to signal the Day of Judgement?",
+    option1: "Jibreel",
+    option2: "Israeel",
+    option3: "Izrafeel",
     answer: 3
 },        
     {
@@ -112,5 +109,79 @@ let myQuestions = [{
     }]
 
 /**
- * function to play the game and show questions and answers
+ * functions to hide divs when playing the game
  */
+document.getElementById('start-button').addEventListener('click', () => {
+    quiz.style.display = 'contents';
+    scoreArea.style.display = 'contents';
+    start.style.display = 'none';
+    startButton.style.display = 'none';
+    result.style.display = 'none';
+});
+
+/*
+ * function to start the game
+ */
+let startGame = () => {
+    questionCounter = 0;
+    score = 0;
+    availableQuestions = [...questions];
+    getNewQuestion();
+};
+
+getNewQuestion = () => {
+    if(availableQuestions.length === 0 || questionCounter > MAX_QUESTIONS) {
+        localStorage.setItem('mostRecentScore', score);
+
+        return scoreArea.style.display = 'contents', quiz.style.display = 'none';
+    }
+
+/**
+ * getting random questions from the array of questions and making sure
+ * that we don't get the same question/answer more than one time
+ */
+    questionCounter++;
+    progressText.innerText = `Question ${questionCounter} of ${MAX_QUESTIONS}`;
+    const questionsIndex = Math.floor(Math.random() * availableQuestions.length);
+    currentQuestion = availableQuestions[questionsIndex];
+    question.innerText = currentQuestion.question;
+    
+/**
+ * defining which choices the user should get within the array of questions
+ * and 
+ */
+    choices.forEach(choice => {
+        const number = choice.dataset.number;
+        choice.innerText = currentQuestion['choice' + number];
+    });
+    availableQuestions.splice(questionsIndex, 1);
+    acceptingAnswers = true;
+};
+
+choices.forEach(choice => {
+    choice.addEventListener('click', e => {
+        if (!acceptingAnswers) return;
+        acceptingAnswers = false;
+        const selectedChoice = e.target;
+        const selectedAnswer = selectedChoice.dataset.number;
+        let classToApply = selectedAnswer == currentQuestion.answer ? 'correct' : 'incorrect';
+        const correctAnswer = choices[currentQuestion.answer -1];
+        if (classToApply === 'correct') {
+            incrementScore(SCORE_POINTS);
+        } else if (classToApply === 'incorrect') {
+            correctAnswer.classList.add('correct');
+        }
+        selectedChoice.classList.ass(classToApply);
+    });
+});
+
+/**
+ * next question button
+ */
+document.getElementById('next-button').addEventListener('click', () => {
+    choices.forEach(choice => {
+        choice.classList.remove('incorrect');
+        choice.classList.remove('correct');
+    })
+    getNewQuestion();
+});
