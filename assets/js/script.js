5daa6509471bccebeebd5159e957891af960d936
mristimaki <rosanna.ristimaki@gmail.com>
2023-03-18 18:30:17 +0000

    Changed html file for quiz area to get all of the content with js instead. Also updated array of Q&A and created new function to game. Not working correctly yet.

--- assets/js/script.js
+++ assets/js/script.js
@@ -6,17 +6,11 @@ const start = document.getElementById('start');
 const quiz = document.getElementById('quiz');
 const result = document.getElementById('result');
 const startButton = document.getElementById('start-button');
-const questionQue = document.getElementById('question');
-const choiceQue = document.querySelectorAll('choices');
 const scoreArea = document.getElementById('score-area');
 const currentScore = document.getElementById('current-score');
 const totalScore = document.getElementById('total-score');
 const playAgain = document.getElementById('play-again');
 
-let currentQuestion = 0;
-let score = 0;
-const SCORE_POINTS = 1;
-const MAX_QUESTIONS = 10;
  /* 
  * hide quiz and result area when window is loaded and 
  * display the score-area
@@ -26,7 +20,7 @@ const MAX_QUESTIONS = 10;
     result.style.display = 'none';
     scoreArea.style.display = 'none';
     playAgain.style.display = 'none';
-;}
+}
 
 /**
  * functions to hide divs when playing the game
@@ -46,115 +40,146 @@ document.getElementById('start-button').addEventListener('click', () => {
  */
 let myQuestions = [
     {
+        id: 1,
         question: 'The word "Al-Yaom" (day) is mentioned in the Quran..',
-        answers: [
-            {option: '1000 times.', answer: false},
-            {option: '365 times.', answer: true},
-            {option: '500 times.', answer:false},
-        ],
+        answer: '365 times.',
+        options: [
+            '1000 times.',
+            '365 times.',
+            '500 times.'
+        ]
     },       
     {
+        id: 2,
         question: 'The word "Shahr" (month) is mentioned in the Quran..',
-        answers: [
-            {option: '33 times.', answer: false},
-            {option: '99 times.', answer: false},
-            {option: '12 times.', answer:true},
-        ],
+        answer: '12 times.',
+        options: [ 
+            '33 times.',
+            '99 times.',
+            '12 times.',
+        ]
     },
     {
+        id: 3,
         question: 'The correct stages of development of the embryo was first mentioned..',
-        answers: [ 
-            {option: 'By Hans Adolf Eduard Driesch 1890.', answer: false},
-            {option: 'In the Quran over 1400 years ago.', answer: true},
-            {option: 'By Frances Maitland Balfour 1880.', answer:false},
+        answer: 'In the Quran over 1400 years ago.',
+        options: [ 
+            'By Hans Adolf Eduard Driesch 1890.',
+            'In the Quran over 1400 years ago.',
+            'By Frances Maitland Balfour 1880.',
         ],
     },
     {
+        id: 4,
         question: 'Allah mentions men and women in the Quran..',
-        answers: [
-            {option: 'Men more than women.', answer: false},
-            {option: 'Women more than men.', answer: false},
-            {option: 'Exactly equal.', answer:true},
+        answer: 'Exactly equal.',
+        options: [
+            'Men more than women.',
+            'Women more than men.',
+            'Exactly equal.'
         ],
     },
     {
+        id: 5,
         question: 'The expansion theory was first mentioned..',
-        answers: [
-            {option: 'By Edwin Hubble 1929.', answer: false},
-            {option: 'By Georges Lemaître 1920.', answer: false},
-            {option: 'In the Quran over 1400 years ago (surah,verse 51:47).', answer:true},
+        answer: 'In the Quran over 1400 years ago (surah,verse 51:47).',
+        options: [
+            'By Edwin Hubble 1929.',
+            'By Georges Lemaître 1920.',
+            'In the Quran over 1400 years ago (surah,verse 51:47).'
         ],
     },
     {
+        id: 6,
         question: 'The word "Islam" means..',
-        answers: [
-            {option: 'One who willfully submits (to God).', answer: true},
-            {option: 'To strive.', answer: false},
-            {option: 'Followers of Prophet Muhammad (ﷺ).', answer:false},
+        answer: 'One who willfully submits (to God).',
+        options: [
+            'One who willfully submits (to God).',
+            'To strive.',
+            'Followers of Prophet Muhammad (ﷺ).'
         ],
     },
     {
+        id: 7,
         question: 'The word "Jihad" means..',
-        answers: [
-            {option: 'Holy war.', answer: false},
-            {option: 'To "struggle" or to "strive".', answer: true},
-            {option: 'Martyrdom.', answer:false},
+        answer: 'To "struggle" or to "strive".',
+        options: [
+            'Holy war.',
+            'To "struggle" or to "strive".',
+            'Martyrdom.'
         ],
     },
     {
+        id: 8,
         question: 'Prophets is mentioned (by name) in the Quran..',
-        answers: [
-            {option: '25.', answer: true},
-            {option: '313.', answer: false},
-            {option: '597.', answer:false},
+        answer: '25.',
+        options: [
+            '25.',
+            '313.',
+            '597.'
         ],
     },
     {
+        id: 9,
         question: 'The angel who will blow the horn to signal the Day of Judgement is..',
-        answers: [
-            {option: 'Izrafeel.', answer: true},
-            {option: 'Mikaeel.', answer: false},
-            {option: 'Jibreel.', answer:false},
+        answer: 'Izrafeel.',
+        options: [
+            'Izrafeel.',
+            'Mikaeel.',
+            'Jibreel.'
         ],
     },
     {
+        id: 10,
         question: 'A muslim should love (after Allah and His Messenger ﷺ )..',
-        answers: [
-            {option: 'His mother three times over before his father.', answer: true},
-            {option: 'His father three times over before his mother.', answer: false},
-            {option: 'His father and mother equally.', answer:false},
+        answer: 'His mother three times over, before his father.',
+        options: [
+            'His mother three times over, before his father.',
+            'His father three times over, before his mother.',
+            'His father and mother equally.'
         ],
     }];
 
-/**
- * function for next-button
- */
-document.getElementById('next-button').addEventListener('click', () => {
-    counter++;
-    display.innerText = myQuestions[counter];
-});
+let questionCount = 0;
+let score = 0;
 
-/**
- * function to start the game and get the Q&A from array
- */
-let answers = [];
-for (let i = 0; i < myQuestions.length; i++) {
-    answers.push({answered: false, correct: null, answer: null});
+function startGame(count) {
+    questionCount = 0;
+    let question = document.getElementById('quiz');
+    let [first, second, third] = myQuestions[count].options;
+
+    question.innerHTML = `<p class="header">${count + 1}. ${myQuestions[count].question}</p>`
+    `<p class="choices">${first}</p>
+    <p class="choices">${second}</p>
+    <p class="choices">${third}</p>`;
+
+    choicesActive();
 }
 
-function startGame() {
-    currentQuestion = 0;
-    score = 0;
-
-    playGame();
-}
-
-function playGame() {
-    questionQue.innerHTML = myQuestions[currentQuestion].question;
-
-    for (let i = 0; i < choiceQue.length; i++) {
-        const choiceButton = choiceQue[i];
-        choiceButton.lastElementChild.innerHTML = myQuestions[currentQuestion].answers[i].option;
+function choicesActive() {
+    let option = document.querySelectorAll('p.choices');
+    for (let i = 0; i < option.length; i++){
+        option[i].onclick = function() {
+            for (let i = 0; i < option.length; i++){
+                if (option[i].classList.contains('active')){
+                    option[i].classList.remove('active');
+                }
+            }
+            option[i].classList.add('active');
+        };
     }
 }
 
+function next(){
+    if(questionCount == myQuestions.length -1) {
+        quiz.style.display = 'none';
+        scoreArea.style.display = 'none';
+        start.style.display = 'none';
+        startButton.style.display = 'none';
+        result.style.display = 'contents';
+    }
+    console.log(questionCount);
+}
+
+console.log(startGame);
+console.log(activeButtons);
