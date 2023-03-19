147a764c3f8fdbebc752bfe28ffdd142a36e988b
mristimaki <rosanna.ristimaki@gmail.com>
2023-03-19 19:19:52 +0000

    Changed text content in Q&A and trying to fix calculate answers function, not working yet.

--- assets/js/script.js
+++ assets/js/script.js
@@ -22,7 +22,7 @@ const playAgain = document.getElementById('play-again');
     result.style.display = 'none';
     scoreArea.style.display = 'none';
     playAgain.style.display = 'none';
-}
+};
 
 /**
  * functions to hide divs when playing the game
@@ -44,81 +44,81 @@ let myQuestions = [
     {
         question: 'The word "Al-Yaom" (day) is mentioned in the Quran..',
         options: [
-            {option: '1000 times.', answer: false},
-            {option: '365 times.', answer: true},
-            {option: '500 times.', answer: false}
+            {option: '1000 times', answer: false},
+            {option: '365 times', answer: true},
+            {option: '500 times', answer: false}
         ],
     },       
     {
         question: 'The word "Shahr" (month) is mentioned in the Quran..',
         options: [ 
-            {option: '33 times.', answer: false},
-            {option: '99 times.', answer: false},
-            {option: '12 times.', answer: true}
+            {option: '33 times', answer: false},
+            {option: '99 times', answer: false},
+            {option: '12 times', answer: true}
         ],
     },
     {
         question: 'The correct stages of development of the embryo was first mentioned..',
         options: [ 
-            {option: 'By Hans Adolf Eduard Driesch 1890.', answer: false},
-            {option: 'In the Quran over 1400 years ago. (surah: verses 23:12-14)', answer: true},
-            {option: 'By Frances Maitland Balfour 1880.', answer: false}
+            {option: 'By Hans Adolf Eduard Driesch 1890', answer: false},
+            {option: 'In the Quran over 1400 years ago (surah: verses 23:12-14)', answer: true},
+            {option: 'By Frances Maitland Balfour 1880', answer: false}
         ],
     },
     {
         question: 'Allah mentions men and women in the Quran..',
         options: [
-            {option: 'Men more than women.', answer: false},
-            {option: 'Women more than men.', answer: false},
-            {option: 'Exactly equal.', answer: true}
+            {option: 'Men more than women', answer: false},
+            {option: 'Women more than men', answer: false},
+            {option: 'Exactly equal', answer: true}
         ],
     },
     {
         question: 'The expansion theory was first mentioned..',
         options: [
-            {option: 'By Edwin Hubble 1929.', answer: false},
-            {option: 'By Georges Lemaître 1920.', answer: false},
-            {option: 'In the Quran over 1400 years ago (surah:verse 51:47).', answer: true}
+            {option: 'By Edwin Hubble 1929', answer: false},
+            {option: 'By Georges Lemaître 1920', answer: false},
+            {option: 'In the Quran over 1400 years ago (surah:verse 51:47)', answer: true}
         ],
     },
     {
         question: 'The word "Islam" means..',
         options: [
-            {option: 'One who willfully submits (to God).', answer: true},
-            {option: 'To strive.', answer: false},
-            {option: 'Followers of Prophet Muhammad (ﷺ).', answer: false}
+            {option: 'One who willfully submits (to God)', answer: true},
+            {option: 'To strive', answer: false},
+            {option: 'Followers of Prophet Muhammad (ﷺ)', answer: false}
         ],
     },
     {
         question: 'The word "Jihad" means..',
         options: [
-            {option: 'Holy war.', answer: false},
-            {option: 'To "struggle" or to "strive".', answer: true},
-            {option: 'Martyrdom.', answer: false}
+            {option: 'Holy war', answer: false},
+            {option: 'To "struggle" or to "strive"', answer: true},
+            {option: 'Martyrdom', answer: false}
         ],
     },
     {
         question: 'Prophets is mentioned (by name) in the Quran..',
         options: [
-            {option: '25.', answer: true},
-            {option: '313.', answer: false},
-            {option: '597.', answer: false}
+            {option: '25', answer: true},
+            {option: '3', answer: false},
+            {option: '9', answer: false}
         ],
     },
     {
         question: 'The angel who will blow the horn to signal the Day of Judgement is..',
         options: [
-            {option: 'Izrafeel.', answer: true},
-            {option: 'Mikaeel.', answer: false},
-            {option: 'Jibreel.', answer: false}
+            {option: 'Izrafeel', answer: true},
+            {option: 'Mikaeel', answer: false},
+            {option: 'Jibreel', answer: false}
         ],
     },
     {
         question: 'A muslim should love (after Allah and His Messenger ﷺ )..',
         options: [
-            {option: 'His mother three times over, before his father.', answer: true},
-            {option: 'His father three times over, before his mother.', answer: false},
-            {option: 'His father and mother equally.', answer: false}
+            {option: 'His mother three times over, before his father', answer: true},
+            {option: 'His father three times over, before his mother', answer: false},
+            {option: 'His father and mother equally', answer: false}
         ],
     }];
 
@@ -135,10 +135,13 @@ for (let i = 0; i < myQuestions.length; i++) {
 
 
 function startGame() {
+
     displayQuestions();
+
 }
 
 function displayQuestions() {
+
     //display question from array
     textQuestion.innerHTML = myQuestions[currentQuestion].question;
     //display options from array
@@ -148,12 +151,39 @@ function displayQuestions() {
     }
 }
 
+
+/**
+ * checking if the answer is true/false
+ */
+function checkAnswer() {
+    
+    let userAnswer = parseInt.document.getElementById('total-score');
+    let isCorrect = userAnswer === calculatedAnswer();
+
+    if (isCorrect) {
+        alert('You got it right!');
+    } else {
+        alert(`Sorry, you answered ${userAnswer}. The correct answer was ${calculatedAnswer}`)
+    }
+}
+
 /**
  * go to next question 
  */
 function next(){
+
     if(currentQuestion<9) {
         currentQuestion++;
         displayQuestions();
     }
+}
+
+/**
+ * gets the current score from DOM and increments it by 1
+ */
+function incrementScore() {
+
+    let oldScore = parseInt(document.getElementById('current-score').innerText);
+    document.getElementById('current-score').innerText = ++oldScore;
+
 }
